ba21c1e7ab15f876f6dc5d1adb68e3b7071b7b44
Android Studio <devandroid@ort>
2025-05-05 12:04:49 -0300

    Avances

--- App.js
+++ App.js
@@ -1,10 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+
+const frases = [
+  "¡Hola, mundo!",
+  "Matteria: DAI",
+  "Profe: Jesi",
+  "React Native",
+  "Hola Vito"
+];
+
+const useGenerateRandomColor = () => {                              //ESTO FUE SACADO DE LA WEB
+  const [color, setColor] = useState("000000");
+  const generateColor = () => {
+    const randomColor = Math.floor(Math.random() * 0xffffff)
+      .toString(16)
+      .padStart(6, "0");
+    setColor(randomColor);
+  };
+  return { color, generateColor };
+};
 
 export default function App() {
+  const { color, generateColor } = useGenerateRandomColor();
+  const [texto, setTexto] = useState(frases[0]);
+
+  const changeText = () => {                                          //ESTO FUE SACADO DE LA WEB
+    const randomIndex = Math.floor(Math.random() * frases.length);
+    setTexto(frases[randomIndex]);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={[styles.txt, { color: `#${color}` }]}>{texto}</Text>
+      <View style={styles.buttonContainer}>
+        <Button onPress={generateColor} title="Cambiar Color" color={`#${color}`} />
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button onPress={changeText} title="Cambiar Texto" color={`#${color}`} />
+      </View>
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +48,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
+    gap: 12,
+  },
+  txt: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  buttonContainer: {
+    marginVertical: 6,
+    width: 200,
   },
 });
